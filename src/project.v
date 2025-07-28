ba101686c76b5f744f01a92d64be7e501044e6d1
Aria M <ariamitra99@yahoo.com>
2025-07-28 14:09:59 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,9 +17,11 @@ module tt_um_musiccc (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+  assign uo_out[4:0] = 0;
+  musicfeatures my_music( .clk1(ui_in[7]), .clk2(ui_in[6]), .octave_dena(ui_in[5]), .octave_uena(ui_in[4]), .tremolo_ena(ui_in[3]), .led_ena(ui_in[2]), .out_o(uo_out[7]), .extra_features(uo_out[6]), .led_out(uo_out[5]));
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
