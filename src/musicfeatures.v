efae035f3e57c01d2618b74e83d409ae9d71a348
Aria M <ariamitra99@yahoo.com>
2025-07-28 14:01:28 -0500

    Update musicfeatures.v

--- src/musicfeatures.v
+++ src/musicfeatures.v
@@ -3,23 +3,23 @@ module musicfeatures (input clk1, input clk2, input octave_dena, input octave_ue
 	wire tremolo;
 	wire octave_up;
 	wire led;
-  wire led1, led2, led3, led4, led5, led6;
+  	wire led1, led2, led3, led4, led5, led6;
 	assign tremolo = clk1&~clk2;
 	assign octave_up = ((~clk1) ^ clk1));
 	DFF v1 ( .D(~octave_down), .clk(clk1), .Q(octave_down) );
 
-  DFF l1 (.D(~led6), .clk(clk1), .Q(led1));
-  DFF l2 (.D(led1), .clk(clk1), .Q(led2));
-  DFF l3 (.D(led2), .clk(clk1), .Q(led3));
-  DFF l4 (.D(led3), .clk(clk1), .Q(led4));
-  DFF l5 (.D(led4), .clk(clk1), .Q(led5));
-  DFF l6 (.D(led5), .clk(clk1), .Q(led6));
-  assign led = led6;
+  	DFF l1 (.D(~led6), .clk(clk1), .Q(led1));
+  	DFF l2 (.D(led1), .clk(clk1), .Q(led2));
+  	DFF l3 (.D(led2), .clk(clk1), .Q(led3));
+  	DFF l4 (.D(led3), .clk(clk1), .Q(led4));
+  	DFF l5 (.D(led4), .clk(clk1), .Q(led5));
+  	DFF l6 (.D(led5), .clk(clk1), .Q(led6));
+  	assign led = led6;
 
 	
-  assign out_o = octave_dena ? octave_down: octave_uena ? octave_up: 0;
-  assign extra_features = tremolo_ena ? tremolo: 0;
-  assign led_out = led_ena ? led: 0;
+  	assign out_o = octave_dena ? octave_down: octave_uena ? octave_up: 0;
+  	assign extra_features = tremolo_ena ? tremolo: 0;
+  	assign led_out = led_ena ? led: 0;
 endmodule
 
 
