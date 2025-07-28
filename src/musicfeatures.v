1e7e7c116226749d7f37317ef86f2b6999948e17
Aria M <ariamitra99@yahoo.com>
2025-07-28 17:22:05 -0500

    Update musicfeatures.v

--- src/musicfeatures.v
+++ src/musicfeatures.v
@@ -1,9 +1,7 @@
-module musicfeatures (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, input led_ena, output out_full, output led_out);
+module top_module (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, input led_ena, output out_full, output led_out);
 	wire octave_down;
 	wire tremolo;
 	wire octave_up;
-	wire led;
-	wire led1, led2, led3, led4, led5, led6;
 	wire octave_change;
 	wire octave_on;
 	wire octave_out;
@@ -18,17 +16,13 @@ module musicfeatures (input clk1, input clk2, input octave_dena, input octave_ue
 	assign tremolo = octave_out & ~ clk2;
 	assign out_full = tremolo_ena ? tremolo: octave_out;
 
-	DFF l1 (.D(~led6), .clk(octave_out), .Q(led1));
-	DFF l2 (.D(led1), .clk(~led1), .Q(led2));
-	DFF l3 (.D(led2), .clk(~led2), .Q(led3));
-	DFF l4 (.D(led3), .clk(~led3), .Q(led4));
-	DFF l5 (.D(led4), .clk(~led4), .Q(led5));
-	DFF l6 (.D(led5), .clk(~led5), .Q(led6));
-	assign led = led6;
-
-	assign led_out = led_ena ? led: 0;
+	reg [5:0] q;
+	reg led;
+	reg led_reg;
+	always @(posedge clk1) begin
+		q <= (q==63) ? 0: q+1;
+		led <= (q == 0) ? 0:1;
+		led_reg <= led_ena ? led: 0;
+	end
+	assign led_out = led_reg;
 endmodule
-
-
-
-
