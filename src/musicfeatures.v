d8751f14efda3b171986df19c8d546bdccaa4c49
Aria M <ariamitra99@yahoo.com>
2025-07-28 19:04:03 -0500

    Update musicfeatures.v

--- src/musicfeatures.v
+++ src/musicfeatures.v
@@ -1,20 +1,12 @@
-module musicfeatures (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, input led_ena, output out_full);
+module top_module (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, output out_o, output extra_features);
 	wire octave_down;
 	wire tremolo;
 	wire octave_up;
-	wire octave_change;
-	wire octave_on;
-	wire octave_out;
-
+	assign tremolo = clk1&~clk2;
 	assign octave_up = ((~clk1) ^ clk1);
 	DFF v1 ( .D(~octave_down), .clk(clk1), .Q(octave_down) );
 
-	assign octave_on = octave_dena ^ octave_uena;
 	
-	assign octave_change = octave_dena ? octave_down: octave_uena ? octave_up: 0;
-	assign octave_out = octave_on ?  octave_change : clk1;
-	assign tremolo = octave_out & ~ clk2;
-	assign out_full = tremolo_ena ? tremolo: octave_out;
-
-	
+	assign out_o = octave_dena ? octave_down: octave_uena ? octave_up: 0;
+	assign extra_features = tremolo_ena ? tremolo: 0;
 endmodule
