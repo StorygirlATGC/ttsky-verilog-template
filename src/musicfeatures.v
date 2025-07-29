41f243d4960d460ed89fc37e2bec149faabaef92
Aria M <ariamitra99@yahoo.com>
2025-07-28 19:30:48 -0500

    Update musicfeatures.v

--- src/musicfeatures.v
+++ src/musicfeatures.v
@@ -1,12 +1,29 @@
-module musicfeatures (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, output out_o, output extra_features);
+module musicfeatures (input clk1, input clk2, input octave_dena, input octave_uena, input tremolo_ena, input led_ena, output out_full, output led_out);
 	wire octave_down;
 	wire tremolo;
 	wire octave_up;
-	assign tremolo = clk1&~clk2;
+	wire octave_change;
+	wire octave_on;
+	wire octave_out;
+
 	assign octave_up = ((~clk1) ^ clk1);
 	DFF v1 ( .D(~octave_down), .clk(clk1), .Q(octave_down) );
 
+	assign octave_on = octave_dena ^ octave_uena;
 	
-	assign out_o = octave_dena ? octave_down: octave_uena ? octave_up: 0;
-	assign extra_features = tremolo_ena ? tremolo: 0;
+	assign octave_change = octave_dena ? octave_down: octave_uena ? octave_up: 0;
+	assign octave_out = octave_on ?  octave_change : clk1;
+	assign tremolo = octave_out & ~clk2;
+	assign out_full = tremolo_ena ? tremolo: octave_out;
+
+	reg [5:0] q;
+	reg led;
+	reg led_reg;
+	always @(posedge clk1) begin
+		q <= (q==63) ? 0: q+1;
+		led <= (q == 0) ? 0:1;
+		led_reg <= led_ena ? led: 0;
+	end
+	assign led_out = led_reg;
 endmodule
+
